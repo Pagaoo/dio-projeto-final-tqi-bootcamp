a2b17e4b25bc1f59527788d6b781245da8981ff5
Pagão <gabrielpaganinibarroso@hotmail.com>
2023-12-11 16:53:25 -0300

    commit inicial com configuração do banco de dados h2

---
+++ src/main/kotlin/pagao/creditapi/CreditapiApplication.kt
@@ -0,0 +1,11 @@
+package pagao.creditapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditapiApplication>(*args)
+}
---
+++ src/test/kotlin/pagao/creditapi/CreditapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package pagao.creditapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
